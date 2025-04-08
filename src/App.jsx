fae7b44fb82c66343d155f42c8b34be3a471bf71
bharanimanikandan <kskaranbharani2001@gmail.com>
2025-04-08 21:27:42 +0530

    Vijay push

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import Footer from "./Components/Footer";
 import ScrollToTopButton from "./Components/ScrollToTopButton";
 import SampleProjects from "./Pages/SampleProjects";
+import Services from "./Pages/Services";
 
 function App() {
   return (
     <div>
       <ScrollToTopButton />
       <SampleProjects />
-
+      <Services />
       <Footer />
     </div>
   );
