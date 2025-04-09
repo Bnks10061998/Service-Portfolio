4017ee470df5ac3cefb55fcfe2ae4b58713bae99
vijaya-kumar789 <vijaykumaryoyo8@gmail.com>
2025-04-09 15:15:40 +0530

    update app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import "./App.css";
+import ScrollToTopButton from "./Components/ScrollToTopButton";
 import SampleProjects from "./Pages/SampleProjects";
+import Footer from "./Components/Footer";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
   return (
