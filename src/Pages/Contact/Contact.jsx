507a1036b6159204d0d3310c193916920c430955
vijaya-kumar789 <vijaykumaryoyo8@gmail.com>
2025-04-10 16:47:54 +0530

    updated contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,33 +1,72 @@
 import React from "react";
 import "./contact.css";
 import { motion } from "framer-motion";
+import { useFormik } from "formik";
+import emailjs from "@emailjs/browser";
+import { SERVICE_ID,TEMPLATE_ID, PUBLIC_KEY } from "../../../utils/config";
+import contactAnimation from "./animation.json"
+import Lottie from "lottie-react";
 
 const Contact = () => {
+
+
+  const onSubmit = async (values, actions) => {
+    try {
+      await emailjs.send(
+        SERVICE_ID,
+        TEMPLATE_ID,
+        values,
+        PUBLIC_KEY,
+      );
+      alert("Message sent successfully!");
+      actions.resetForm()
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const { values, handleChange,errors, handleSubmit } = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    },
+    onSubmit,
+  });
+
   return (
     <div className="container-fluid py-5 d-flex justify-content-center align-items-center">
       <div
-        className="container bg-white shadow rounded-4 p-4 p-md-5 position-relative ball-wrapper"
+        className="container bg-white shadow rounded-4 p-2 p-md-5 position-relative ball-wrapper"
         style={{ maxWidth: "960px", zIndex: 1 }}
       >
+    
         <div
           className="row justify-content-center align-items-center"
-          style={{ columnGap: "80px" }}
+          
         >
           {/* Left side: Contact Form */}
           <motion.div
            
-            initial={"hidden"}
-        whileInView={"visible"}
-        variants={{
-          visible: {
-            x: -10,
-            opacity: 1,
-            transition: {
-              type: "spring",
-            },
-          },
-          hidden: { opacity: 0, x: -300 },
-        }}
+           initial="hidden"
+           whileInView="visible"
+           variants={{
+             visible: {
+               x: -10,
+               opacity: 1,
+               transition: {
+                 type: "spring",
+                 stiffness: 50,    // lower = softer spring
+                 damping: 15,      // higher = less bounce
+                 duration: 1.2     // optional, but makes it feel smoother
+               },
+             },
+             hidden: {
+               opacity: 0,
+               x: -300,
+             },
+           }}
             className="col-md-5 mb-4 mb-md-0 position-relative"
             style={{ zIndex: 1 }}
           >
@@ -35,17 +74,30 @@ const Contact = () => {
             <p className="text-muted text-center mb-4">
               Feel free to contact us anytime.
             </p>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="inputBox">
-                <input required type="text" placeholder=" " />
+                <input required type="text" placeholder=" " id="name"
+                      onChange={ handleChange}
+                      value={values.name} />
                 <span>First name</span>
               </div>
               <div className="inputBox">
-                <input required type="email" placeholder=" " />
+                <input required type="email" placeholder=" " id="email"
+                      onChange={ handleChange}
+                      aria-describedby="emailHelp"
+                      value={values.email} />
                 <span>Email</span>
               </div>
               <div className="inputBox">
-                <textarea required placeholder=" " rows="3"></textarea>
+                <input required type="number" placeholder=" " id="phone"
+                      onChange={ handleChange}
+                      aria-describedby="phone"
+                      value={values.phone} />
+                <span>Phone</span>
+              </div>
+              <div className="inputBox">
+                <textarea required placeholder=" " rows="3"  id="message"
+                      value={values.message} onChange={handleChange}></textarea>
                 <span>Message</span>
               </div>
 
@@ -78,47 +130,22 @@ const Contact = () => {
             opacity: 1,
             transition: {
               type: "spring",
+              stiffness: 50,    // lower = softer spring
+                 damping: 15,      // higher = less bounce
+                 duration: 1.2   
             },
           },
           hidden: { opacity: 0, x: 300 },
         }}
-            style={{
-              backgroundColor: "#003a84",
-              borderTopLeftRadius: "5px",
-              borderBottomRightRadius: "5px",
-              borderTopRightRadius: "50px",
-              borderBottomLeftRadius: "50px",
-              zIndex: 1,
-            }}
+           
           >
-            <h5 className="fw-bold mb-4">Info</h5>
-            <p>
-              <i className="bi bi-envelope-fill me-2"></i> info@getintouch.we
-            </p>
-            <p>
-              <i className="bi bi-telephone-fill me-2"></i> +24 56 89 146
-            </p>
-            <p>
-              <i className="bi bi-geo-alt-fill me-2"></i> 14 Greenroad St.
-            </p>
-            <p>
-              <i className="bi bi-clock-fill me-2"></i> 09:00 - 18:00
-            </p>
-
-            <div className="mt-4">
-              <a href="#" className="text-white me-3">
-                <i className="bi bi-globe"></i>
-              </a>
-              <a href="#" className="text-white me-3">
-                <i className="bi bi-instagram"></i>
-              </a>
-              <a href="#" className="text-white">
-                <i className="bi bi-link-45deg"></i>
-              </a>
-            </div>
+           
+            <div >
+        <Lottie animationData={contactAnimation} loop={true} style={{ width: "150%", maxWidth: "600px", height: "auto", transform: "translateX(-10%)" }} />
+      </div>
           </motion.div>
         </div>
-
+        
         {/* Small decorative balls */}
         <div className="ball top-left-ball"></div>
         <div className="ball bottom-right-ball"></div>
